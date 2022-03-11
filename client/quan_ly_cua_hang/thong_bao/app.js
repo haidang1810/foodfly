7798fc0e9ddd7ee2295ce2de5054b4b35719f42e
DangHuynh <danghuynh.dev@gmail.com>
2022-03-11 23:09:54 +0700

    them page ql thong bao

---
+++ client/quan_ly_cua_hang/thong_bao/app.js
@@ -0,0 +1,8 @@
+$("#all-nofi").click(function(){
+    $("#unseen-nofi").removeClass('button-active');
+    $(this).addClass('button-active');
+})
+$("#unseen-nofi").click(function(){
+    $("#all-nofi").removeClass('button-active');
+    $(this).addClass('button-active');
+})
